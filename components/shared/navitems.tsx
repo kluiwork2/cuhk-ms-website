737a2eb34d6ada412a9d18f8020d1c8bae008a65
unknown <klui.work2@gmail.com>
2024-10-03 17:53:15 +0800

    fix wrong change

--- components/shared/navitems.tsx
+++ components/shared/navitems.tsx
@@ -31,11 +31,11 @@ const NavItems = () => {
             <DropdownMenuItem>
               <Link href={"/general/obese"}>超重或肥胖</Link>
             </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href={"/general/smoke"}>吸煙</Link>
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-      </li>
-      <li>
-        <Link href={"/general/smoke"}>吸煙</Link>
       </li>
       <li>
         <Link href={"/general/sports"}>做運動</Link>
